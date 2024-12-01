Fixed reward model functionality to allow for full action prediction gradients

collect_rewards.py
@@ -1,4 +1,4 @@
-# Usage: python collect_rewards.py --n_inits 1000 --model_path ./data/checkpoints/pusht_state_policy_ep100_pretrained42.ckpt --seed 42
+# Usage: python collect_rewards.py --n_inits 300 --model_path ./data/checkpoints/pusht_state_policy_ep100_pretrained42.ckpt --seed 42
 
 import torch
 import random
@@ -59,9 +59,10 @@ def collect_rewards(n_inits, model_path, seed):
     # get environment
     env = PushTEnv(kp_obs=True)
     
-    # define data arrays that we will populate 
-    obs_action_data = np.zeros(((max_steps // action_horizon) * n_inits, obs_dim + action_dim * action_horizon), dtype=np.float32) # (38 * n_inits, 36)
-    reward_data = np.zeros(((max_steps // action_horizon) * n_inits, 2), dtype=np.float32) # (38 * n_inits, 2)
+    # define data array that we will populate
+    action_reward_data = np.zeros(((max_steps // action_horizon) * n_inits, 
+                                   obs_dim + pred_horizon * action_dim + 2), 
+                                  dtype=np.float32) # (38 * n_inits, 54)
 
     # collect data by doing n_init episodes
     n_policy_evals = 0
@@ -75,7 +76,6 @@ def collect_rewards(n_inits, model_path, seed):
         obs_deque = collections.deque(
             [obs] * obs_horizon, maxlen=obs_horizon)
         
-        done = False
         step_idx = 0
 
         with tqdm(total=max_steps, desc=f"Collecting Rewards for Init {i}") as pbar:
@@ -115,19 +115,18 @@ def collect_rewards(n_inits, model_path, seed):
                         ).prev_sample
 
                 # unnormalize action
-                naction = naction.detach().to('cpu').numpy()
-                # (B, pred_horizon, action_dim)
+                naction = naction.detach().to('cpu').numpy() # (B, pred_horizon, action_dim)
                 naction = naction[0]
-                action_pred = unnormalize_data(naction, stats=stats['action']) # list of shape (16, 2)
+                action_pred = unnormalize_data(naction, stats=stats['action']) # (pred_horizon, action_dim)
 
                 # only take action_horizon number of actions
                 start = obs_horizon - 1
                 end = start + action_horizon
                 action = action_pred[start:end,:] # (action_horizon, action_dim)
 
-                # collect reward data inputs 
-                obs_action_data[n_policy_evals, :20] = obs_seq[-1]
-                obs_action_data[n_policy_evals, 20:] = np.array(action).flatten()
+                # collect action data 
+                action_reward_data[n_policy_evals, :obs_dim] = obs_seq[-1]
+                action_reward_data[n_policy_evals, obs_dim:-2] = np.array(action_pred).flatten()
 
                 # execute action_horizon number of steps
                 # without replanning
@@ -148,8 +147,8 @@ def collect_rewards(n_inits, model_path, seed):
                     
                 # collect reward data labels 
                 dense_rewardT = env.dense_reward()
-                reward_data[n_policy_evals, 0] = dense_rewardT
-                reward_data[n_policy_evals, 1] = dense_rewardT - dense_reward0
+                action_reward_data[n_policy_evals, -2] = dense_rewardT
+                action_reward_data[n_policy_evals, -1] = dense_rewardT - dense_reward0
                 n_policy_evals += 1
             
         # logging 
@@ -157,14 +156,13 @@ def collect_rewards(n_inits, model_path, seed):
         wandb.log({'n_policy_evals': n_policy_evals})
         wandb.log({'step_idx': n_policy_evals})
 
-    np.save(f'./data/reward/obs_action_data_{n_inits}inits.npy', obs_action_data)
-    np.save(f'./data/reward/reward_data_{n_inits}inits.npy', reward_data) 
+    np.save(f'./data/reward/action_reward_data_{n_inits}inits.npy', action_reward_data)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     
-    parser.add_argument('--n_inits', default=1000, type=int)
+    parser.add_argument('--n_inits', default=300, type=int)
     parser.add_argument('--model_path', default='./data/checkpoints/pusht_state_policy_ep100_pretrained42.ckpt', type=str)
     parser.add_argument('--seed', default=42, type=int)
     parsed_args = parser.parse_args()

diffusion_policy/dataset/reward_dataset.py
@@ -5,18 +5,20 @@ from diffusion_policy.dataset.normalization import get_data_stats, normalize_dat
 
 
 class RewardDataset(torch.utils.data.Dataset):
-    def __init__(self, obs_actions_path, rewards_path, train=True, reward_diff=True):
+    def __init__(self, action_reward_path, train=True, reward_diff=True):
         
         # load data
-        obs_action_data = np.load(obs_actions_path)
+        action_reward_data = np.load(action_reward_path)
+        
+        # separate actions and rewards
+        obs_action_data = action_reward_data[:, :-2]
+        reward_data = action_reward_data[:, -2:][:, int(reward_diff)]
         self.stats = get_data_stats(obs_action_data)
         
-        obs_action_data = torch.tensor(normalize_data(obs_action_data, self.stats), dtype=torch.float32) # (N, 36)
-        reward_data = torch.tensor(np.load(rewards_path)[:, int(reward_diff)], dtype=torch.float32) # (N,)
-
-        assert obs_action_data.shape[0] == reward_data.shape[0], 'obs_action_data and reward_data must have the same length'        
+        obs_action_data = torch.tensor(normalize_data(obs_action_data, self.stats), dtype=torch.float32) # (N, 52)
+        reward_data = torch.tensor(reward_data, dtype=torch.float32) # (N,)
         
-        # split into train or test
+        # take train or test subset 
         split_idx = int(0.8 * len(obs_action_data))
         self.obs_action_data = obs_action_data[:split_idx] if train else obs_action_data[split_idx:]
         self.reward_data = reward_data[:split_idx] if train else reward_data[split_idx:]

diffusion_policy/model/reward.py
@@ -5,7 +5,7 @@ class RewardModel(nn.Module):
     def __init__(self, reward_diff=True):
         super().__init__()
 
-        self.net = nn.Sequential(nn.Linear(36, 16),
+        self.net = nn.Sequential(nn.Linear(52, 16),
                                  nn.SiLU(),
                                  nn.Linear(16, 16),
                                  nn.SiLU(),

train_reward.py
@@ -1,4 +1,4 @@
-# Usage: python train_reward.py --num_epochs 100 --lr 1e-4 --batch_size 256 --reward_diff --obs_actions_path ./data/reward/obs_action_data_300inits.npy --rewards_path ./data/reward/reward_data_300inits.npy --seed 42
+# Usage: python train_reward.py --num_epochs 100 --lr 1e-2 --batch_size 256 --reward_diff --action_reward_path ./data/reward/action_reward_data_300inits.npy --seed 42
 import torch
 import torch.nn as nn
 import numpy as np 
@@ -10,7 +10,7 @@ from diffusion_policy.dataset.reward_dataset import RewardDataset
 from diffusion_policy.model.reward import RewardModel
 
 
-def train(num_epochs, lr, batch_size, reward_diff, obs_actions_path, rewards_path, seed):
+def train(num_epochs, lr, batch_size, reward_diff, action_reward_path, seed):
     # set seeds
     torch.manual_seed(seed)
     np.random.seed(seed)
@@ -19,8 +19,7 @@ def train(num_epochs, lr, batch_size, reward_diff, obs_actions_path, rewards_pat
     
     # load data
     train_dataset = RewardDataset(
-        obs_actions_path=obs_actions_path, 
-        rewards_path=rewards_path, 
+        action_reward_path=action_reward_path,
         train=True,
         reward_diff=reward_diff)
     
@@ -33,8 +32,7 @@ def train(num_epochs, lr, batch_size, reward_diff, obs_actions_path, rewards_pat
         persistent_workers=True)
     
     test_dataset = RewardDataset(
-        obs_actions_path=obs_actions_path, 
-        rewards_path=rewards_path, 
+        action_reward_path=action_reward_path,
         train=False,
         reward_diff=reward_diff)
     
@@ -132,11 +130,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--num_epochs', default=100, type=int)
-    parser.add_argument('--lr', default=1e-22, type=float)
+    parser.add_argument('--lr', default=1e-2, type=float)
     parser.add_argument('--batch_size', default=256, type=int)
     parser.add_argument('--reward_diff', action='store_true')
-    parser.add_argument('--obs_actions_path', default='./data/reward/obs_action_data_300inits.npy', type=str)
-    parser.add_argument('--rewards_path', default='./data/reward/reward_data_300inits.npy', type=str)
+    parser.add_argument('--action_reward_path', default='./data/reward/action_reward_data_300inits.npy', type=str)
     parser.add_argument('--seed', default=42, type=int)
     parsed_args = parser.parse_args()
     
@@ -147,6 +144,5 @@ if __name__ == "__main__":
           lr=parsed_args.lr,
           batch_size=parsed_args.batch_size,
           reward_diff=parsed_args.reward_diff,
-          obs_actions_path=parsed_args.obs_actions_path,
-          rewards_path=parsed_args.rewards_path,
+          action_reward_path=parsed_args.action_reward_path,
           seed=parsed_args.seed)